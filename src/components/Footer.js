ce40e8567377260a968eb759315066d2b2b5407a
madeInAba <ezeokecc@gmail.com>
2019-07-09 12:00:07 +0100

    header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+return(
+    <div>
+        <div>
+        Get access to all offers, discounts and deals!
+    </div>
+    <div id='foot'>
+      <div>
+      <div> 
+           <h3>LET US HELP YOU</h3>
+        <p>Help Center</p>
+        <p>Warranty</p>
+        <p>Service Centers</p>
+        <p>Contact Us</p>
+        <p>How to Shop 0n jumia</p>
+        <p>Shipping & Delivery</p>
+        <p>Return Policy</p>
+        <p>Corporate & Bulk Purchase</p>
+        <p>Jumia Mobile Apps</p>
+        <p>Jumia Bot(Beta)</p>
+        </div>
+      <div>
+          <h3>MAKE MONEY WITH US</h3>
+         <p>Sell on Jumia</p>
+          <p>Become an Affiliate Partner</p>
+          <p>Become a Sales Consultant</p>
+          <p>Become a Jumia Vendor Service Provider</p>
+          <p></p>
+      </div>
+      </div>
+      <div>
+          <h3>GET TO KNOW US</h3>
+      <p>Careers</p>
+      <p>About Us</p>
+      <p>Jumia Express</p>
+      <p>Shipped From Overseas</p>
+      <p>Jumia Prime </p>
+      <p>Terms and Conditions</p>
+      <p>Privacy Policy</p>
+      <p>Economy Postal Shipping</p>
+      </div>
+
+     <div>
+         <h3>JUMIA INTERNATIONAL</h3>
+         <p>Algeria</p>
+         <p>Cameroun</p>
+         <p>Egypt</p>
+         <p>Ghana</p>
+         <p>Ivory Coast</p>
+         <p>Kenya</p>
+         <p>Morocco</p>
+         <p>Senegal</p>
+         <p>Tanzania</p>
+         <p>Tunisia</p>
+         <p>Uganda</p>
+     </div>
+     <div>
+       <h3>JOIN US ON</h3>  
+       <h3>CONTACT US ON</h3>
+       <p>01 888 1100/0700 600 0000</p>
+       <h3>PAYMENT METHODS & DELIVERY PARTNER</h3>
+       <h3>JUMIA GROUP</h3>                                                                                                                                                                                                                                                                                                                                                                                                                                    
+     </div>
+    </div>
+    </div>
+)
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import mtn from './pictures/mtn.png';
+import axa from './pictures/axa-mansard-2.png';
+import jumia from './pictures/jumialogonew.png';
+import tv from './pictures/dreamtv.jpg';
+import sell from './pictures/sell.png';
+import pay from './pictures/Pay-on-delivery.png';
+import easy from './pictures/Easy-return-(1).png';
+import delivery from './pictures/delivery.jpg';
+import './Header.css'
+
+function Header() {
+
+ return (
+  <div id='header'>
+   <div id='header1'>
+    <span>In Partnership with</span> <img src={mtn} alt='mtnLogo'/> &
+  <img src={axa} alt="axa-mansard" />
+   </div>
+
+   <div id='header2'>
+
+    <div id='header2i'>
+     <img src={jumia} alt="" />
+   <div id='input'>
+   <input type="text" placeholder='Search for products, brands and categories' />
+    <button>SEARCH</button>
+   </div>
+     <div id='need'>
+     <p>Need <br /> <span>Help?</span> </p>
+    <p>Hello <br /> <span>Chike</span> </p>
+     </div>
+    </div>
+
+   </div>
+
+   <div id='header3'>
+    <div id='one'>
+     <p>FREE DELIVERY</p>
+     <p>PHONES AND TABLETS</p>
+     <p>COMPUTING</p>
+     <p>ELECTRONICS</p>
+     <p>HOME OFFICE</p>
+     <p>FASHION</p>
+     <p>HEALTH & BEAUTY</p>
+     <p>GAMING</p>
+     <p>GROCERY</p>
+     <p>GLOBAL STORE</p>
+     <p>BABY PRODUCTS</p>
+     <p>OTHER CATEGORIES</p>
+    </div>
+    <div id='delivery'>
+     <img src={tv} alt="" />
+    </div>
+    <div id='one1'>
+     <div id='sell'>
+      <div className='buy' id='buy1'> 
+        <img src={sell} alt="jumia sell" />
+      <p> SELL ON JUMIA <br/>
+      <span>Millions of Monthly Visitors</span>  </p> 
+      </div>
+      <div className='buy'>
+      <img src={pay} alt="jumia pay" />
+      <p>PAY ON DELIVERY <br/>
+      <span>After Checking the Product</span> </p>
+      </div>
+       <div className='buy'>
+       <img src={easy} alt="easy return" />
+       <p>
+       EASY RETURN <br/>
+        <span>Quick Refund</span>  
+       </p>
+       </div>
+     </div>
+     <div id='modal'>
+      <img src={delivery} alt="modal" />
+     </div>
+    </div>
+   </div>
+  </div>
+ )
+}
+
+export default Header;
